d7e347cff36817eec63ddcde05cb54a86f76ee58
Moeez Tariq <73531265+moeezgujr@users.noreply.github.com>
2022-05-11 00:33:45 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./component/header";
+import Home from "./component/home";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
